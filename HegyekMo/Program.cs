3c8e9b43c558d855057b07ff993af605ab5b3727
Orlando-R <roglorand@gmail.com>
2021-04-24 00:36:53 +0200

    Hegyek2- osszes, legmagasabb

--- HegyekMo/Program.cs
+++ HegyekMo/Program.cs
@@ -35,6 +35,29 @@ namespace HegyekMo
             }
             sr.Close();
 
+            Console.WriteLine($"3. feladat: Hegycsúcsok száma: {hegyek.Count} db");
+
+            //4 feladat
+
+            double osszmagassag = 0;
+            foreach (Hegy h in hegyek)
+            {
+                osszmagassag += h.magassag;
+            }
+
+            Console.WriteLine($"4. feladat: Hegycsúcsok átlagos magassága: {osszmagassag/hegyek.Count} m");
+
+            //5 feladat
+            Hegy legmagasabb = new Hegy();
+            foreach (Hegy h in hegyek)
+            {
+                if (h.magassag > legmagasabb.magassag)
+                {
+                    legmagasabb = h;
+                }
+            }
+            Console.WriteLine($"5. feladat: A legmagasabb hegycsúcs adatai:\n\tNév: {legmagasabb.hegycsucs}\n\tHegység: {legmagasabb.hegyseg}\n\tMagasság: {legmagasabb.magassag}");
+
             Console.ReadLine();
 
         }
